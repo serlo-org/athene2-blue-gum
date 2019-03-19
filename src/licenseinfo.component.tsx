437f009592d118a269396e9a22962f3b727bb476
elpoto <1258870+elbotho@users.noreply.github.com>
2019-03-19 17:55:41 +0100

    sorry for the mess ¯\_(ツ)_/¯

--- src/licenseinfo.component.tsx
+++ src/licenseinfo.component.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { lighten } from 'polished'
-import { Anchor } from "grommet";
-import SVG from 'react-inlinesvg';
+import { Anchor } from 'grommet'
+import SVG from 'react-inlinesvg'
 const licenseSymbols = require('./img/license_ccbysa.svg')
-
 
 //needs license info page URLs
 
@@ -12,38 +11,47 @@ export default class LicenseInfo extends React.Component {
   public render() {
     return (
       <LicenseWrap>
-        Diese Inhalte stehen unter der freien <Anchor href="https://creativecommons.org/licenses/by-sa/4.0/deed.de" rel="license nofollow">CC-BY-SA 3.0 Lizenz</Anchor>, wenn nicht anders angeben.<br/>
-        Was das genau bedeutet erfährst du <Anchor href="#">hier</Anchor>.<br/>
-        <IconAnchor href="https://creativecommons.org/licenses/by-sa/4.0/deed.de"><SVG src={licenseSymbols}/></IconAnchor>
+        Diese Inhalte stehen unter der freien{' '}
+        <Anchor
+          href="https://creativecommons.org/licenses/by-sa/4.0/deed.de"
+          rel="license nofollow"
+        >
+          CC-BY-SA 3.0 Lizenz
+        </Anchor>
+        , wenn nicht anders angeben.
+        <br />
+        Was das genau bedeutet erfährst du <Anchor href="#">hier</Anchor>.<br />
+        <IconAnchor href="https://creativecommons.org/licenses/by-sa/4.0/deed.de">
+          <SVG src={licenseSymbols} />
+        </IconAnchor>
       </LicenseWrap>
     )
   }
 }
 
-
 const LicenseWrap = styled.div(props => {
-  const lightBlue = lighten(0.2,props.theme.global.colors.brand)
-  return `
+  const lightBlue = lighten(0.2, props.theme.global.colors.brand)
+  return css`
     opacity: 0.8;
-    padding: .5rem 0;
+    padding: 0.5rem 0;
     margin-bottom: 2rem;
-    border-top: 1px solid ${ lightBlue };
-    color: ${ lightBlue };
-    fill: ${ lightBlue };
+    border-top: 1px solid ${lightBlue};
+    color: ${lightBlue};
+    fill: ${lightBlue};
   `
 })
 
 const IconAnchor = styled(Anchor)(props => {
-  const lightBlue = lighten(0.2,props.theme.global.colors.brand)
-  return `
+  const lightBlue = lighten(0.2, props.theme.global.colors.brand)
+  return css`
     display: inline-block;
-    margin-top: .6rem;
+    margin-top: 0.6rem;
     &:hover {
       background: none;
       box-shadow: none;
     }
     span > svg {
-      fill: ${ lightBlue };
+      fill: ${lightBlue};
       width: 6rem;
       height: auto;
     }
