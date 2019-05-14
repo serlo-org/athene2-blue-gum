692e317966c8e349ceb59d79fe7d57a40d46f04c
David Li <dawei.li@gmx.net>
2019-05-14 13:29:51 +0200

    added button and responsivenesss

--- src/courseoverview.component.tsx
+++ src/courseoverview.component.tsx
@@ -2,9 +2,15 @@ import * as React from 'react'
 import { getColor } from './provider.component'
 import { Icon } from './icon.component'
 import { Heading } from './heading.component'
-import { List } from './list.component'
+import useWindowSize from '@rehooks/window-size'
+import { MobileMenuButton } from './header/mobilemenubutton'
+import { Button } from './button.component'
 
 export function CourseOverview(props) {
+  const windowSize = useWindowSize()
+  const showFull = windowSize.innerWidth > 550 ? true : false
+  let [isExpanded, setExpanded] = React.useState(false)
+
   return (
     <div
       style={{
@@ -19,17 +25,30 @@ export function CourseOverview(props) {
         seiner Umrechnung
         <br />
       </Heading>
-      <ol>
-        <li>Einleitung</li>
-        <li>Prozente aus dem Alltag</li>
-        <li>Prozent - Eine neue Zahldarstellung</li>
-        <li>Prozentzahlen über 100%</li>
-        <li>Umformungen von Prozentzahlen und Dezimalzahlen</li>
-        <li>
-          Übungsaufgaben zur Umrechnung von Prozentzahlen und Dezimalzahlen
-        </li>
-        <li>Umrechnung von Prozentzahlen und Bruchzahlen</li>
-      </ol>
+      {isExpanded || showFull ? (
+        <ol>
+          <li>Einleitung</li>
+          <li>Prozente aus dem Alltag</li>
+          <li>Prozent - Eine neue Zahldarstellung</li>
+          <li>Prozentzahlen über 100%</li>
+          <li>Umformungen von Prozentzahlen und Dezimalzahlen</li>
+          <li>
+            Übungsaufgaben zur Umrechnung von Prozentzahlen und Dezimalzahlen
+          </li>
+          <li>Umrechnung von Prozentzahlen und Bruchzahlen</li>
+        </ol>
+      ) : (
+        <Button
+          iconName="faBars"
+          label="Kursübersicht anzeigen"
+          secondary
+          color="lighterblue"
+          style={{ marginBottom: '3rem' }}
+          onClick={() => {
+            setExpanded(!isExpanded)
+          }}
+        />
+      )}
     </div>
   )
 }
